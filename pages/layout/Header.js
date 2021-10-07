76bc2f09c6386102ae88f0830ab8f3da101d6401
Muhammad Fajri <fajrijijin@gmail.com>
2021-10-07 23:39:54 +0700

    first

---
+++ pages/layout/Header.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div>
+      <Link href="/">
+        <h1>Header</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
---
+++ pages/route.js
@@ -0,0 +1,9 @@
+const Route = () => {
+  return (
+    <div>
+      <h1>Hello Route</h1>
+    </div>
+  );
+};
+
+export default Route;
